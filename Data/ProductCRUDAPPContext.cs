b2f63b3e17e4f6022bd7473a06ae1c399aaa9d28
Mihai-Alexandru Istrate <mihai.istrate@student.unitbv.ro>
2020-05-22 02:12:45 +0300

    Add project files.

---
+++ Data/ProductCRUDAPPContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductCRUDAPP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductCRUDAPP.Data
+{
+    public class ProductCRUDAPPContext : DbContext
+    {
+        public ProductCRUDAPPContext(DbContextOptions<ProductCRUDAPPContext> options)
+            : base(options) { }
+        public DbSet<Product> Product { get; set; }
+
+    }
+}
---
+++ Models/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProductCRUDAPP.Models;
+using ProductCRUDAPP.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProductCRUDAPP.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+
+            using var context = new ProductCRUDAPPContext(serviceProvider.GetRequiredService<DbContextOptions<ProductCRUDAPPContext>>());
+            if (context.Product.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Product.AddRange(
+                new Product
+                {
+                    Name = "Apple",
+                    Price = 2,
+                    Quantity = 1
+
+                },
+                new Product
+                {
+                    Name = "Orange",
+                    Price = 3,
+                    Quantity = 2
+                },
+                new Product
+                {
+                    Name = "Banana",
+                    Price = 5,
+                    Quantity = 4
+                }
+
+
+                );
+            context.SaveChanges();
+            // Look for any movies.
+
+
+        }
+        
+    }
+    
+}
